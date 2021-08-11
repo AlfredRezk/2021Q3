72d8eb889a5fc38ca45b284d70185e98bc6344c3
AlfredRezk <73665419+AlfredRezk@users.noreply.github.com>
2021-08-11 18:35:50 -0400

    uploading first lecture files

---
+++ PI1/01.Introduction-to-programming/Lecture/currency-convertor-nodejs/convertor.js
@@ -0,0 +1,9 @@
+
+function usdToEuro(dollars){
+
+  let euro = parseFloat(dollars) * 0.883795087;
+  console.log(`Euros = ${euro} `)
+}
+
+
+usdToEuro("5")
---
+++ PI1/01.Introduction-to-programming/Lecture/example.js
@@ -0,0 +1,40 @@
+// variables
+
+// let base = 2;
+// let height = 8;
+
+// console.log(base * height / 2)
+
+// loop from 1-100
+// for (let a = 1; a <= 100; a++) {
+//   console.log(a);
+// }
+
+// printing variables 
+
+// let size = 5;
+
+// console.log('The box size is ' + size + 'meter')  // The box size is 5
+
+// console.log(`The box size is ${size} meter`);
+// console.log(`The Area = ${size * size} meter square`); 
+
+
+// Defining the function
+function main() {
+  let size = 5;
+  console.log(`The box size is ${size} meter`);
+	console.log(`The Area = ${size * size} meter square`);
+
+  size = 7 
+}
+
+// Calling a function
+main()
+
+
+
+
+
+
+
