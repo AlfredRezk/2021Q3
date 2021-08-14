e827479ddd6776d72dd694b975fe07d851974c73
AlfredRezk <73665419+AlfredRezk@users.noreply.github.com>
2021-08-14 11:12:52 -0400

    Delete PI1 directory

--- PI1/01.Introduction-to-programming/Lecture/currency-convertor-nodejs/convertor.js
+++
@@ -1,9 +0,0 @@
-
-function usdToEuro(dollars){
-
-  let euro = parseFloat(dollars) * 0.883795087;
-  console.log(`Euros = ${euro} `)
-}
-
-
-usdToEuro("5")
--- PI1/01.Introduction-to-programming/Lecture/currency-convertor/index.js
+++
@@ -1,8 +0,0 @@
-
-function usdToEuro() {
-	let userInput = document.getElementById("dollars").value;
-	let dollars = Number(userInput); // "15" -> 15  "Kingsland" -> NaN
-	let euros = dollars * 0.883795087;
-
-	document.getElementById("euros").value = euros;
-}
