7768f75f3517975848d8a840b62f0d63661bac80
Alfred Rezk <73665419+AlfredRezk@users.noreply.github.com>
2021-10-26 11:21:28 -0400

    Add files via upload

--- PI3/03.Arrays/08.diagonal-sum.js
+++ PI3/03.Arrays/08.diagonal-sum.js
@@ -1,26 +1,26 @@
 function main(inputArr) {
-  // create arrays 
+
+  // create empty arrays 
   let mainArr = [];
   let secArr = [];
-
-  // define starting index 
   let mainIndex = 0;
   let secIndex = inputArr[0].length - 1;
-
   // Loop through every array 
-  inputArr.forEach(row => {
 
-    // Push the main diagonal number to mainArr 
+  inputArr.forEach(row => {
     mainArr.push(row[mainIndex]);
     secArr.push(row[secIndex]);
     mainIndex++;
     secIndex--;
-  })
+  });
 
-let mainSum = mainArr.reduce((sum, num)=> sum+num, 0)
-let secSum = secArr.reduce((sum, num)=> sum+num, 0)
+  // add the mainArr, secArr array items
+  
+  let mainSum = 0;
+  mainArr.forEach(num => mainSum += num);
 
-  console.log(`${mainSum} ${secSum}`)
+  let secSum = secArr.reduce((sum, num) => sum + num, 0);
+  console.log(mainSum+' '+ secSum)
 }
 
 
---
+++ PI3/03.Arrays/09.equal-neighboors.js
@@ -0,0 +1,25 @@
+function main(arr) {
+
+  let count = 0;
+
+  for (let row = 0; row < arr.length -1 ; row++) {
+    // check if the row has a neighbor array 
+    // if (arr[row + 1]) {
+      for (let col = 0; col < arr[row].length; col++) {
+  
+        if (arr[row][col] === arr[row + 1][col]) {
+          count++;
+        }
+      }
+    }
+
+  // }
+  console.log(count)
+}
+
+
+main([
+	["test", "yes", "yo", "ho"],
+	["well", "done", "yo", "6"],
+	["not", "done", "yet", "5"],
+]);
